ab1ff2b7b8d45fd37fae4ab2a5f4eb13f0482f13
SilentCodeSamurai <silentcodesamurai@gmail.com>
2024-09-09 21:03:23 +0300

    fix env

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	define: {
-		"import.meta.env.SHOP_API_URL": JSON.stringify(process.env.BACKEND_URL || "http://127.0.0.1/api"),
+		"import.meta.env.SHOP_API_URL": JSON.stringify(process.env.SHOP_API_URL || "http://127.0.0.1/api"),
 		"import.meta.env.AUTH_API_URL": JSON.stringify(process.env.AUTH_API_URL || "http://127.0.0.1/auth"),
 	},
 });
