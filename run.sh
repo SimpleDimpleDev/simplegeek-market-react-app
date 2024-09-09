934cbc8b1214997922a54dc377779da1e7057b55
SilentCodeSamurai <silentcodesamurai@gmail.com>
2024-09-09 20:51:33 +0300

    networking

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -it --network intranet -p 3001:3001 simplegeek-react-app-server
+docker run -it --network intranet --network-alias simplegeek-react-app-server -p 3030:3030 simplegeek-react-app-server
