86411053e766ed50586727c061d637d97c55fb9e
SilentCodeSamurai <silentcodesamurai@gmail.com>
2024-09-09 21:06:08 +0300

    docker run name

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -it --network intranet --network-alias simplegeek-react-app-server -p 3030:3030 simplegeek-react-app-server
+docker run -d --name simplegeek-react-app-server --network intranet --network-alias simplegeek-react-app-server -p 3030:3030 simplegeek-react-app-server
