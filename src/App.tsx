f61110de11324a207c68b2fb54f380a71c0c585e
SilentCodeSamurai <silentcodesamurai@gmail.com>
2024-09-20 17:30:00 +0300

    fix: Ory elements styles added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import "./App.css";
+import "@ory/elements/style.css";
 import { bootstrapStore } from "@state/bootstrap";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
 import { AppDispatch } from "./state/store";
 import { useDebouncedResizeHandler } from "./hooks/useDebouncedResizeHandler";
+import { fetchCatalogItemsAvailability } from "@state/shop/thunks";
 
 function App() {
 	const dispatch = useDispatch<AppDispatch>();
 	useDebouncedResizeHandler();
-	
+
 	useEffect(() => {
 		bootstrapStore(dispatch);
+		const revalidateAvailabilityInterval = setInterval(() => {
+			dispatch(fetchCatalogItemsAvailability());
+		}, 60000);
+		return () => clearInterval(revalidateAvailabilityInterval);
 	}, [dispatch]);
 
 	return <Outlet />;
