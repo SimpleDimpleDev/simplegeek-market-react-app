aae235368263f058d726c9078426756d8eca0683
SilentCodeSamurai <silentcodesamurai@gmail.com>
2024-10-06 00:36:47 +0300

    api connected to store

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,24 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import { shopApi } from "@api/shop/root";
 import userAuthorityReducer from "./user/authoritySlice";
 import responsiveReducer from "./ui/responsiveSlice";
 
-import {
-	crashReporterMiddleware,
-	loggingMiddleware,
-	updateUserItemsMiddleware,
-} from "./middleware";
+import { crashReporterMiddleware, loggingMiddleware, updateUserItemsMiddleware } from "./middleware";
 
 const store = configureStore({
 	reducer: {
+		[shopApi.reducerPath]: shopApi.reducer,
 		userAuthority: userAuthorityReducer,
 		responsive: responsiveReducer,
 	},
 	middleware: (getDefaultMiddleware) =>
 		getDefaultMiddleware()
+			.concat(shopApi.middleware)
 			.concat(loggingMiddleware)
 			.concat(crashReporterMiddleware)
-			.concat(updateUserItemsMiddleware)
+			.concat(updateUserItemsMiddleware),
 });
 
 export { store };
