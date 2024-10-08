2dfa1b9a350a5db914ab60d3f30fbde4ef1917fc
SilentCodeSamurai <silentcodesamurai@gmail.com>
2024-10-09 00:22:44 +0300

    Discount fix

--- src/schemas/CatalogItem.ts
+++ src/schemas/CatalogItem.ts
@@ -4,17 +4,12 @@ import { CreditInfoSchema } from "./Payment";
 import { IdSchema, ISOToDateSchema } from "./Primitives";
 import { ProductShopSchema } from "./Product";
 
-export const DiscountSchema = z.object({
-	type: z.enum(["FIXED", "PERCENTAGE"]),
-	value: z.number(),
-});
-
 export const CatalogItemShopSchema = z.object({
 	id: IdSchema,
 	product: ProductShopSchema,
 	rating: z.number(),
 	price: z.number(),
-	discount: DiscountSchema.nullable(),
+	discount: z.number().nullable(),
 	variationIndex: z.number().nullable(),
 	creditInfo: CreditInfoSchema.nullable(),
 	createdAt: ISOToDateSchema,
