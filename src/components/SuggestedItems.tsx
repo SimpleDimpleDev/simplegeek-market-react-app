92b48b412695788e60ca5eefcf0d1a14f41a6964
SilentCodeSamurai <silentcodesamurai@gmail.com>
2024-10-09 19:32:31 +0300

    Suggested?

--- src/components/SuggestedItems.tsx
+++ src/components/SuggestedItems.tsx
@@ -1,78 +1,80 @@
-// import { ChevronLeft, ChevronRight } from "@mui/icons-material";
-// import { Box, IconButton, Typography, styled } from "@mui/material";
-// import { createRef } from "react";
+import { ChevronLeft, ChevronRight } from "@mui/icons-material";
+import { Box, IconButton, Typography, styled } from "@mui/material";
+import { ArrowProps } from "react-multi-carousel/lib/types";
 
-// import ItemCard from "./ItemCard";
-// import { Loading } from "./Loading";
-// import { useGetCatalogQuery } from "@api/shop/catalog";
-// import { useIsMobile } from "src/hooks/useIsMobile";
+import ItemCard from "./ItemCard";
+import { Loading } from "./Loading";
+import { useGetCatalogQuery } from "@api/shop/catalog";
+import { useIsMobile } from "src/hooks/useIsMobile";
 
-// const ItemBar = styled(Box)({
-// 	display: "flex",
-// 	alignItems: "center",
-// 	position: "relative",
-// 	overflowX: "auto",
-// 	margin: "10px 0",
-// 	gap: 8,
-// 	scrollbarWidth: "none",
-// 	"&::-webkit-scrollbar": {
-// 		display: "none",
-// 	},
-// });
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
-// const ScrollButton = styled(IconButton)({
-// 	position: "absolute",
-// 	top: "38%",
-// 	transform: "translateY(-50%)",
-// 	zIndex: 2,
-// 	boxShadow: "0px 2px 18px -8px #000000",
-// 	backgroundColor: "white",
-// 	"&&:hover": {
-// 		backgroundColor: "white",
-// 	},
-// });
+const responsive = {
+	superLargeDesktop: {
+		// the naming can be any, depends on you.
+		breakpoint: { max: 4000, min: 3000 },
+		items: 5,
+	},
+	desktop: {
+		breakpoint: { max: 3000, min: 1024 },
+		items: 3,
+	},
+	tablet: {
+		breakpoint: { max: 1024, min: 464 },
+		items: 2,
+	},
+	mobile: {
+		breakpoint: { max: 464, min: 0 },
+		items: 1,
+	},
+};
+const ScrollButton = styled(IconButton)({
+	position: "absolute",
+	top: "38%",
+	transform: "translateY(-50%)",
+	zIndex: 2,
+	boxShadow: "0px 2px 18px -8px #000000",
+	backgroundColor: "white",
+	"&&:hover": {
+		backgroundColor: "white",
+	},
+});
+
+const LeftButton = ({ onClick, ...rest }: ArrowProps) => (
+	<ScrollButton {...rest} onClick={onClick}>
+		<ChevronLeft />
+	</ScrollButton>
+);
+
+const RightButton = ({ onClick, ...rest }: ArrowProps) => (
+	<ScrollButton {...rest} onClick={onClick}>
+		<ChevronRight />
+	</ScrollButton>
+);
 
 export default function SuggestedItems() {
-	// const scrollRef = createRef<HTMLDivElement>();
+	const isMobile = useIsMobile();
 
-	// const isMobile = useIsMobile();
-
-	// const { data: catalog, isLoading: catalogIsLoading } = useGetCatalogQuery();
-
-	// const scroll = (direction: "left" | "right") => {
-	// 	if (scrollRef.current) {
-	// 		const scrollAmount = direction === "left" ? -1420 : 1420;
-	// 		scrollRef.current.scrollBy({ left: scrollAmount, behavior: "smooth" });
-	// 	}
-	// };
+	const { data: catalog, isLoading: catalogIsLoading } = useGetCatalogQuery();
 
 	return (
-		<>
-		</>
-		// <Box padding={"40px 0 24px 0"} width={"100%"} gap={3}>
-		// 	<Typography variant="h5">Также может понравится</Typography>
-		// 	<Loading isLoading={catalogIsLoading} necessaryDataIsPersisted={!!catalog}>
-		// 		<Box display="flex" alignItems="center" position={"relative"}>
-		// 			{!isMobile && (
-		// 				<ScrollButton sx={{ left: "-2%" }} onClick={() => scroll("left")}>
-		// 					<ChevronLeft />
-		// 				</ScrollButton>
-		// 			)}
-		// 			<ItemBar ref={scrollRef} style={{}}>
-		// 				{/* TODO: mechanism for suggesting items */}
-		// 				{catalog?.items.map((data) => (
-		// 					<Box minWidth={346} key={data.id}>
-		// 						<ItemCard data={data} />
-		// 					</Box>
-		// 				))}
-		// 			</ItemBar>
-		// 			{!isMobile && (
-		// 				<ScrollButton sx={{ right: "-2%" }} onClick={() => scroll("right")}>
-		// 					<ChevronRight />
-		// 				</ScrollButton>
-		// 			)}
-		// 		</Box>
-		// 	</Loading>
-		// </Box>
+		<Box padding={"40px 0 24px 0"} width={"100%"} gap={3}>
+			<Typography variant="h5">Также может понравится</Typography>
+			<Loading isLoading={catalogIsLoading} necessaryDataIsPersisted={!!catalog}>
+				<Carousel
+					responsive={responsive}
+					swipeable={isMobile}
+					draggable={true}
+					deviceType={isMobile ? "mobile" : "desktop"}
+					customLeftArrow={<LeftButton />}
+					customRightArrow={<RightButton />}
+				>
+					{catalog?.items.map((item) => (
+						<ItemCard key={item.id} data={item} />
+					))}
+				</Carousel>
+			</Loading>
+		</Box>
 	);
 }
