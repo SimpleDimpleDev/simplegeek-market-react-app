0d67e8c7a99e2cca2b01d353a45bd439087e8011
SilentCodeSamurai <silentcodesamurai@gmail.com>
2024-10-10 15:28:20 +0300

    try design2

--- src/layout/content/DesktopContent.tsx
+++ src/layout/content/DesktopContent.tsx
@@ -31,6 +31,7 @@ const DesktopContent: React.FC = () => {
 							className="pt-4 w-100 h-100 d-f fd-c fg-1 jc-fs"
 							style={{
 								paddingBottom: "28px",
+								minHeight: "100vh",
 								maxWidth: `calc(1392/1920 * 1920px)`,
 							}}
 							ref={nodeRef}
