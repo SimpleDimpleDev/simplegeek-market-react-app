826e3e132be138f00a8d774443c893ba19d10a01
SilentCodeSamurai <silentcodesamurai@gmail.com>
2024-10-13 19:47:16 +0300

    Validation error message fix

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -5,7 +5,7 @@ export const validateData = <T extends ZodSchema>(schema: T, data: unknown): z.i
 		return schema.parse(data);
 	} catch (error) {
 		if (error instanceof ZodError) {
-			console.error("Validation error", { schemaName: Object.keys(schema._def)[0] });
+			console.error("Validation error", { schemaName: Object.keys(schema)[0], schema: schema._def });
 		}
 		throw error;
 	}
