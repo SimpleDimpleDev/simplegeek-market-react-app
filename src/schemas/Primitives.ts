6d51cb8ba8d47ba52909667940a03cba4a7e8c03
SilentCodeSamurai <silentcodesamurai@gmail.com>
2024-10-14 19:45:40 +0300

    Offset added

--- src/schemas/Primitives.ts
+++ src/schemas/Primitives.ts
@@ -2,4 +2,4 @@ import { z } from "zod";
 
 export const IdSchema = z.string({ description: "Id" }).uuid();
 
-export const ISOToDateSchema = z.string({ description: "ISO Date" }).datetime().pipe(z.coerce.date());
+export const ISOToDateSchema = z.string({ description: "ISO Date" }).datetime({ offset: true }).pipe(z.coerce.date());
