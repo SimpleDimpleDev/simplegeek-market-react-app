d028d2f0afe25fcc384feca5742c4c5acf61a2d0
SilentCodeSamurai <silentcodesamurai@gmail.com>
2024-10-14 21:31:15 +0300

    Site opened

--- src/layout/ShopLayout.tsx
+++ src/layout/ShopLayout.tsx
@@ -1,7 +1,5 @@
-import { CircularProgress, Typography } from "@mui/material";
-import { RootState } from "@state/store";
+import { CircularProgress } from "@mui/material";
 import { lazy, Suspense } from "react";
-import { useSelector } from "react-redux";
 import { ScrollRestoration } from "react-router-dom";
 import { useIsMobile } from "src/hooks/useIsMobile";
 
@@ -12,42 +10,8 @@ const DesktopContent = lazy(() => import("./content/DesktopContent"));
 const MobileFooter = lazy(() => import("./footer/MobileFooter"));
 const DesktopFooter = lazy(() => import("./footer/DesktopFooter"));
 
-// only while dev
-// TODO: remove this on release
-import { IntlProvider, CustomTranslations, ThemeProvider as OryThemeProvider } from "@ory/elements";
-import customTranslations from "src/oryLocale";
-import oryTheme from "src/oryTheme.ts";
-import { Component as LoginRoute } from "src/routes/auth/login/LoginRoute";
-
 export default function ShopLayout() {
 	const isMobile = useIsMobile();
-
-	// only while dev
-	// TODO: remove this on release
-	const user = useSelector((state: RootState) => state.user.identity);
-	if (!user) {
-		return (
-			<OryThemeProvider themeOverrides={oryTheme}>
-				<IntlProvider<CustomTranslations>
-					locale="ru"
-					defaultLocale="ru"
-					customTranslations={customTranslations}
-				>
-					<div className="bg-secondary w-100v h-100v ai-c d-f jc-c">
-						<LoginRoute />
-					</div>
-				</IntlProvider>
-			</OryThemeProvider>
-		);
-	}
-
-	if (!user.isAdmin) {
-		return (
-			<div className="w-100v h-100v ai-c d-f fd-r jc-c" style={{ textAlign: "center" }}>
-				<Typography variant="h1">Скоро здесь будет магазин {":)"}</Typography>
-			</div>
-		);
-	}
 
 	return (
 		<div className="d-f fd-c" style={{ height: "100vh" }}>
