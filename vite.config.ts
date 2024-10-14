b641386d286d878558fdc293a23f50f3c5d539d4
SilentCodeSamurai <silentcodesamurai@gmail.com>
2024-10-15 00:15:11 +0300

    sentry init 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig, PluginOption } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -5,19 +6,24 @@ import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		tsconfigPaths(),
-		visualizer({
-			emitFile: true,
-			filename: "stats.html",
-		}) as PluginOption,
-	],
-	optimizeDeps: {
+    plugins: [react(), tsconfigPaths(), visualizer({
+        emitFile: true,
+        filename: "stats.html",
+    }) as PluginOption, sentryVitePlugin({
+        org: "silentcodeops",
+        project: "simplegeek-client"
+    })],
+
+    optimizeDeps: {
 		include: ["@emotion/styled"],
 	},
-	define: {
+
+    define: {
 		"import.meta.env.SHOP_API_URL": JSON.stringify(process.env.SHOP_API_URL || "http://127.0.0.1/api"),
 		"import.meta.env.AUTH_API_URL": JSON.stringify(process.env.AUTH_API_URL || "http://127.0.0.1/auth"),
 	},
+
+    build: {
+        sourcemap: true
+    }
 });
