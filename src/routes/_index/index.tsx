9d5a26fe5980ce9c4fc4dc072ef6fdb05925541c
SilentCodeSamurai <silentcodesamurai@gmail.com>
2025-01-23 13:22:32 +0300

    upd: seo

--- src/routes/_index/index.tsx
+++ src/routes/_index/index.tsx
@@ -15,6 +15,22 @@ export function Component() {
 					name="description"
 					content="Коллекционные статуи, фигурки и саундтреки на виниле из любимых игр и вселенных. SimpleGeek — ваш проводник в мир совершенства форм милых сердцу персонажей по ещё более милым сердцу ценам."
 				/>
+				<script type="application/ld+json">
+					{`{
+						"@context": "https://schema.org",
+						"@type": "WebSite",
+						name: "SimpleGeek",
+						alternateName: ["Симпл Гик", "СимплГик"],
+						url: "https://simplegeek.ru/",
+						description:
+							"Коллекционные статуи, фигурки и саундтреки на виниле из любимых игр и вселенных. SimpleGeek — ваш проводник в мир совершенства форм милых сердцу персонажей по ещё более милым сердцу ценам.",
+						potentialAction: {
+							"@type": "SearchAction",
+							target: "https://simplegeek.ru/search?q={search_term_string}",
+							"query-input": "required name=search_term_string",
+						},
+					}`}
+				</script>
 			</Helmet>
 			<Catalog
 				sectionFilter={sectionFilter}
