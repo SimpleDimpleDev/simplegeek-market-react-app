88fed517e34ebd3bd8aea8fa551166dbc08f7ad0
SilentCodeSamurai <silentcodesamurai@gmail.com>
2025-01-25 00:03:14 +0300

    upd: new badges

--- src/components/Badges.tsx
+++ src/components/Badges.tsx
@@ -1,17 +1,16 @@
 import { OrderStatus } from "@appTypes/Order";
 import { PreorderStatus } from "@appTypes/Preorder";
-import { StatusBadge } from "./StatusBadge";
-import { Tooltip } from "@mui/material";
+import { Chip, Tooltip } from "@mui/material";
 
 export const orderStatusBadges: Record<OrderStatus, JSX.Element> = {
 	CANCELLED: (
 		<Tooltip title={"Заказ отменен."}>
-			<StatusBadge color="icon.secondary">Отменен</StatusBadge>
+			<Chip color="error" label="Отменен" />
 		</Tooltip>
 	),
 	UNPAID: (
 		<Tooltip title={"Заказ не оплачен. Пожалуйста, оплатите заказ."}>
-			<StatusBadge color="icon.attention">Не оплачен</StatusBadge>
+			<Chip color="warning" label="Не оплачен" />
 		</Tooltip>
 	),
 	ACCEPTED: (
@@ -20,52 +19,52 @@ export const orderStatusBadges: Record<OrderStatus, JSX.Element> = {
 				'Заказ оплачен. Ожидайте смены статуса на "Готов к выдаче" или "Передан в доставку" в зависимости от вашего заказа.'
 			}
 		>
-			<StatusBadge color="warning.main">Оформлен</StatusBadge>
+			<Chip color="warning" label="Оформлен" />
 		</Tooltip>
 	),
 	DELIVERY: (
 		<Tooltip title={"Вы можете отследить заказ в службе доставки."}>
-			<StatusBadge color="rgb(0, 175, 134)">Передан в доставку</StatusBadge>
+			<Chip color="info" label="Передан в доставку" />
 		</Tooltip>
 	),
 	READY_FOR_PICKUP: (
 		<Tooltip title={"Заказ ожидает в пункте выдачи."}>
-			<StatusBadge color="icon.success">Готов к выдаче</StatusBadge>
+			<Chip color="success" label="Готов к выдаче" />
 		</Tooltip>
 	),
 	FINISHED: (
 		<Tooltip title={"Спасибо за заказ!"}>
-			<StatusBadge color="icon.success">Выдан</StatusBadge>
+			<Chip color="success" label="Выдан" />
 		</Tooltip>
 	),
 };
 
-export const preorderBadge: JSX.Element = <StatusBadge color="rgb(0, 175, 134)">Предзаказ</StatusBadge>;
+export const preorderBadge: JSX.Element = <Chip color="success" label="Предзаказ" />;
 
 export const preorderStatusBadges: Record<PreorderStatus, JSX.Element> = {
 	WAITING_FOR_RELEASE: (
 		<Tooltip title={"Ожидание производства товаров."}>
-			<StatusBadge color="icon.brandSecondary">Ожидание релиза</StatusBadge>
+			<Chip color="warning" label="Ожидание релиза" />
 		</Tooltip>
 	),
 	FOREIGN_SHIPPING: (
 		<Tooltip title={"Товары в пути на зарубежный склад."}>
-			<StatusBadge color="rgb(0, 175, 134)">Доставка на зарубежный склад</StatusBadge>
+			<Chip color="info" label="Доставка на зарубежный склад" />
 		</Tooltip>
 	),
 	LOCAL_SHIPPING: (
 		<Tooltip title={"Товары в пути на наш склад."}>
-			<StatusBadge color="rgb(0, 175, 134)">Доставка на склад РФ</StatusBadge>
+			<Chip color="info" label="Доставка на склад РФ" />
 		</Tooltip>
 	),
 	DISPATCH: (
 		<Tooltip title={"Товары уже прибыли к нам на склад. Выберите способ доставки, если ещё не сделали этого."}>
-			<StatusBadge color="icon.success">Идёт выдача предзаказов</StatusBadge>
+			<Chip color="success" label="Идёт выдача предзаказов" />
 		</Tooltip>
 	),
 	FINISHED: (
 		<Tooltip title={"Предзаказ завершен."}>
-			<StatusBadge color="icon.success">Завершен</StatusBadge>
+			<Chip color="success" label="Завершен" />
 		</Tooltip>
 	),
 };
