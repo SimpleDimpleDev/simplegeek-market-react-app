2f70a662fa651a1bb7c627f8a22119934f9b5de5
SilentCodeSamurai <silentcodesamurai@gmail.com>
2025-03-10 11:28:22 +0300

    upd: notfound

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,44 +1,51 @@
 import { Button, Typography } from "@mui/material";
+import { Helmet } from "react-helmet";
 import { useNavigate } from "react-router-dom";
 
 export default function NotFound() {
+	const navigate = useNavigate();
 
-    const navigate = useNavigate();
-    
-    return (
-        <div style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            width: "100%",
-            height: "100%",
-        }}>
-            <div style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                gap: 16
-            }}>
-                <Typography variant="h1">
-                    404
-                </Typography>
-                <div style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                }}>
-                    <Typography variant="h6">
-                        Что-то пошло не так
-                    </Typography>
-                    <Typography variant="body1">
-                        Похоже данной страницы не существует
-                    </Typography>
-                </div>
+	return (
+		<>
+			<Helmet>
+				<title>404</title>
+                <meta name="ssr" content="404" />
+			</Helmet>
 
-                <Button variant="contained" onClick={() => navigate("/")}>
-                    На главную
-                </Button>
-            </div>
-        </div>
-    )
+			<div
+				style={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+					width: "100%",
+					height: "100%",
+				}}
+			>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+						gap: 16,
+					}}
+				>
+					<Typography variant="h1">404</Typography>
+					<div
+						style={{
+							display: "flex",
+							flexDirection: "column",
+							alignItems: "center",
+						}}
+					>
+						<Typography variant="h6">Что-то пошло не так</Typography>
+						<Typography variant="body1">Похоже данной страницы не существует</Typography>
+					</div>
+
+					<Button variant="contained" onClick={() => navigate("/")}>
+						На главную
+					</Button>
+				</div>
+			</div>
+		</>
+	);
 }
